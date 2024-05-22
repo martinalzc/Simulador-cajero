c7c97d59b6001775f53a0d5d2a553d6bf0ef65ce
Martina Lazcano <martinalazcano18@gmail.com>
2024-05-22 18:42:14 -0300

    simulador terminado

--- index.js
+++ index.js
@@ -12,7 +12,41 @@ let saldo = 0;
 
 let option;
 
-while (oprion != 0){
-    
-}
+while (option != 0) {
+    option = prompt(`
+    Ingrese una de las siguientes opciones:
+      * 1. Ver saldo
+      * 2. Ingresar dinero
+      * 3. Retirar parcialmente dinero
+      * 4. Retirar todo el dinero
+      * 0. Opción de salida
+    `);
+
+    switch (option) {
+        case "1":
+            alert(saldo);
+            break;
+        case "2":
+            const moneyIn = Number(prompt('Cuanto dinero desea ingresar'));
+            saldo += moneyIn;
+            alert('Dinero ingresado con éxito, su nuevo saldo es: ' + saldo);
+            break;
+        case "3":
+            const moneyOutPartial = Number(prompt('Cuanto dinero desea retirar'));
+            saldo -= moneyOutPartial;
+            alert('Dinero retirado con éxito, su nuevo saldo es: ' + saldo);
+            break;
+        case "4":
+            saldo = 0;
+            alert('Dinero retirado con éxito, su nuevo saldo es: ' + saldo);
+            break;
+        case "0":
+            alert('Gracias, vuelva prontos!');
+            break;
+        default:
+            alert('Ingrese una opción válida');
+            break;
+        }
+    }
+
 
