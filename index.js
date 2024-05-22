17d63f9de0973b3e14009e3f38c7b089d38f9709
Martina Lazcano <martinalazcano18@gmail.com>
2024-05-22 18:12:12 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+//CAJERO AUTOMATICO
+
+/**
+ * 1. Ver saldo
+ * 2. Ingresar dinero
+ * 3. Retirar parcalmente dinero
+ * 4. Retirar todo el dinero
+ * 0. Opcion de salida
+ */
+
+let saldo = 0;
+
+let option;
+
+while (oprion != 0){
+    
+}
+
